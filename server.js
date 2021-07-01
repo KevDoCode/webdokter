e3a0dc50ca579d866a2a969ac62da2c5313e1302
Novandi Kevin Pratama <nkpknight34@gmail.com>
2021-07-02 04:53:57 +0700

    starr pod

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const cors = require("cors");
 app.use(cors({ credentials: true }));
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://webdokter.herokuapp.com"
+  );
 
   // Request methods you wish to allow
   res.setHeader(
